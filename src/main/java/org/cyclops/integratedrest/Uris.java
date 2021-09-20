4024fd53874f62135dea0eccdb435a42767a1738
Ruben Taelman <rubensworks@gmail.com>
2021-09-20 10:49:10 +0200

    Fix incorrect prefixes

--- src/main/java/org/cyclops/integratedrest/Uris.java
+++ src/main/java/org/cyclops/integratedrest/Uris.java
@@ -11,6 +11,6 @@ public class Uris {
     public static final String XSD = "http://www.w3.org/2001/XMLSchema#";
     public static final String XSD_DECIMAL = XSD + "decimal";
     public static final String XSD_LONG = XSD + "long";
-    public static final String IT = "https://raw.githubusercontent.com/CyclopsMC/IntegratedREST/" + MinecraftHelpers.getMinecraftVersionMajorMinor() + "/src/main/resources/";
+    public static final String IT = "https://raw.githubusercontent.com/CyclopsMC/IntegratedREST/master-" + MinecraftHelpers.getMinecraftVersionMajorMinor() + "/src/main/resources/";
 
 }
